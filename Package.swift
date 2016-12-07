fd2664a885550edc4e9a9bc54d20a67ec0d5ce0a
Tony Allevato <tony.allevato@gmail.com>
2016-12-07 12:14:32 -0800

    Bring over conformance test runner from apple/swift-protobuf-test-conformance (#162)
    * Copy conformance test runner into runtime/plugin project.
    * Update package manifest to include Conformance.
    * Update Makefile to regenerate and run conformance tests.

--- Package.swift
+++ Package.swift
@@ -1,14 +1,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftProtobuf",
-    targets: [
-        Target(name: "PluginLibrary", dependencies: ["SwiftProtobuf"]),
-        Target(name: "protoc-gen-swift", dependencies: ["PluginLibrary", "SwiftProtobuf"]),
-    ]
+  name: "SwiftProtobuf",
+  targets: [
+    Target(name: "PluginLibrary",
+           dependencies: ["SwiftProtobuf"]),
+    Target(name: "protoc-gen-swift",
+           dependencies: ["PluginLibrary", "SwiftProtobuf"]),
+    Target(name: "Conformance",
+           dependencies: ["SwiftProtobuf"]),
+  ]
 )
 
 // Ensure that the dynamic library is created for the performance test harness.
 products.append(
-    Product(name: "SwiftProtobuf", type: .Library(.Dynamic), modules: "SwiftProtobuf")
+  Product(name: "SwiftProtobuf",
+          type: .Library(.Dynamic),
+          modules: "SwiftProtobuf")
 )
