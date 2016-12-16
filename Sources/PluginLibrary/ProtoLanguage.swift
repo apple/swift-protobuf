a13bed0f64697a84db16c217307498ee7f9224af
Tim Kientzle <tkientzle@apple.com>
2016-12-16 12:59:18 -0800

    Clarify what this function actually tests and provide a link to the language grammar

--- Sources/PluginLibrary/ProtoLanguage.swift
+++ Sources/PluginLibrary/ProtoLanguage.swift
@@ -17,6 +17,15 @@
 import Swift
 import Foundation
 
+/// Tests whether a string represents a valid identifier in a
+/// proto schema file.  This is based on the proto language grammar
+/// at:
+///
+/// https://developers.google.com/protocol-buffers/docs/reference/proto3-spec
+///
+/// (Note that proto2 and proto3 have exactly the same grammar
+/// productions for identifiers.)
+///
 public func isValidProtobufIdentifier(_ s: String) -> Bool {
     var i = s.characters.makeIterator()
     if let first = i.next() {
