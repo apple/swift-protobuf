921e45f2f8ad0d7a439ff8e586e04fc2ea9e7c77
Tim Kientzle <tkientzle@apple.com>
2016-12-22 16:56:58 -0800

    Comment the copyright fixup script a bit more, remove some dead code

--- CopyrightFixup.sh
+++ CopyrightFixup.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-new_copyright=""
-
 for f in `find . -name '*.swift' -o -name '*.cc' | sed -e 's|./||' | grep -v '.pb.swift'`; do
     if head -n 4 $f | grep 'DO NOT EDIT' > /dev/null; then
         # If the first lines contain 'DO NOT EDIT', then
@@ -11,11 +9,13 @@ for f in `find . -name '*.swift' -o -name '*.cc' | sed -e 's|./||' | grep -v '.p
         # files that we're not even looking at here.
         echo "DO NOT EDIT: $f"
     else
+        tmp=$f~
+        mv $f $tmp
         if head -n 10 $f | grep 'Copyright.*Apple' > /dev/null; then
             # This has a copyright message, update it
-            tmp=$f~
-            mv $f $tmp
+            # Edit the first line to have the correct filename
             head -n 1 $tmp | sed "s|// [^-]* - \(.*\)|// $f - \1|" >$f
+            # Followed by the current copyright text:
             cat <<EOF >>$f
 //
 // Copyright (c) 2014 - 2016 Apple Inc. and the project authors
@@ -24,14 +24,13 @@ for f in `find . -name '*.swift' -o -name '*.cc' | sed -e 's|./||' | grep -v '.p
 // See LICENSE.txt for license information:
 // https://github.com/apple/swift-protobuf/blob/master/LICENSE.txt
 EOF
+            # Followed by the body of the file
             # The copyright message ends at the first blank comment line after
             # the first line containing "LICENSE.txt":
             cat $tmp | sed -n '/LICENSE.txt/,$ p' | sed -n '/^\/\/$/,$ p' >> $f
         else
             # This does not have a copyright message, insert one
             echo "Inserting copyright >> $f"
-            tmp=$f~
-            mv $f $tmp
             cat <<EOF >>$f
 // $f - description
 //
@@ -47,9 +46,3 @@ EOF
         fi
     fi
 done
-
-
-echo <<EOF
-/*
- * DO NOT EDIT.
-EOF
