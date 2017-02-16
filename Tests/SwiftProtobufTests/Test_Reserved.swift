e9729a3f943c58b60f5389657e46a9c429e1c19b
Thomas Van Lenten <thomasvl@google.com>
2017-02-16 11:00:56 -0500

    Tests covering the metadata names.
    Ensure the static properties don't interfere with fields that happen to
    end up with the same name.
    
    While in here beef up the reserved word tests a little.

--- Tests/SwiftProtobufTests/Test_Reserved.swift
+++ Tests/SwiftProtobufTests/Test_Reserved.swift
@@ -20,13 +20,42 @@ import XCTest
 import SwiftProtobuf
 
 class Test_Reserved: XCTestCase {
-    func testEnumPrefix() {
-        XCTAssertEqual(ProtobufUnittest_SwiftReservedTest.Enum.`double`.rawValue, 1)
-        XCTAssertEqual(ProtobufUnittest_SwiftReservedTest.Enum.`class`.rawValue, 3)
-        XCTAssertEqual(ProtobufUnittest_SwiftReservedTest.Enum.self_.rawValue, 5)
+    func testEnumNaming() {
+        XCTAssertEqual(ProtobufUnittest_SwiftReservedTest.Enum.double.rawValue, 1)
+        XCTAssertEqual(ProtobufUnittest_SwiftReservedTest.Enum.double._protobuf_jsonName, "DOUBLE")
         XCTAssertEqual(ProtobufUnittest_SwiftReservedTest.Enum.json.rawValue, 2)
-        XCTAssertEqual(ProtobufUnittest_SwiftReservedTest.classMessage().debugDescription, "SwiftProtobufTests.ProtobufUnittest_SwiftReservedTest.classMessage:\n")
-        XCTAssertEqual(ProtobufUnittest_SwiftReservedTest.isEqualMessage().debugDescription, "SwiftProtobufTests.ProtobufUnittest_SwiftReservedTest.isEqualMessage:\n")
-        XCTAssertEqual(ProtobufUnittest_SwiftReservedTest.TypeMessage().debugDescription, "SwiftProtobufTests.ProtobufUnittest_SwiftReservedTest.TypeMessage:\n")
+        XCTAssertEqual(ProtobufUnittest_SwiftReservedTest.Enum.json._protobuf_jsonName, "JSON")
+        XCTAssertEqual(ProtobufUnittest_SwiftReservedTest.Enum.`class`.rawValue, 3)
+        XCTAssertEqual(ProtobufUnittest_SwiftReservedTest.Enum.`class`._protobuf_jsonName, "CLASS")
+        XCTAssertEqual(ProtobufUnittest_SwiftReservedTest.Enum.___.rawValue, 4)
+        XCTAssertEqual(ProtobufUnittest_SwiftReservedTest.Enum.___._protobuf_jsonName!, "_")
+        XCTAssertEqual(ProtobufUnittest_SwiftReservedTest.Enum.self_.rawValue, 5)
+        XCTAssertEqual(ProtobufUnittest_SwiftReservedTest.Enum.self_._protobuf_jsonName!, "SELF")
+        XCTAssertEqual(ProtobufUnittest_SwiftReservedTest.Enum.type.rawValue, 6)
+        XCTAssertEqual(ProtobufUnittest_SwiftReservedTest.Enum.type._protobuf_jsonName!, "TYPE")
+    }
+
+    func testMessageNames() {
+        XCTAssertEqual(ProtobufUnittest_SwiftReservedTest.classMessage.protoMessageName, "class")
+        XCTAssertEqual(ProtobufUnittest_SwiftReservedTest.isEqualMessage.protoMessageName, "isEqual")
+        XCTAssertEqual(ProtobufUnittest_SwiftReservedTest.TypeMessage.protoMessageName, "Type")
+    }
+
+    func testFieldNamesMatchingMetadata() {
+        // A chunk of this test is just that things compile because it is calling the names
+        // we expect to have generated.
+
+        var msg = ProtobufUnittest_SwiftReservedTest()
+
+        msg.protoMessageName = 1
+        msg.protoPackageName = 2
+        msg.anyTypePrefix = 3
+        msg.anyTypeURL = 4
+
+        msg.isInitialized_p = "foo"
+        msg.hashValue_p = "bar"
+        msg.debugDescription_p = 5
+
+        XCTAssertEqual(msg.debugDescription, "SwiftProtobufTests.ProtobufUnittest_SwiftReservedTest:\nproto_message_name: 1\nproto_package_name: 2\nany_type_prefix: 3\nanyTypeURL: 4\nis_initialized: \"foo\"\nhash_value: \"bar\"\ndebug_description: 5\n")
     }
 }
