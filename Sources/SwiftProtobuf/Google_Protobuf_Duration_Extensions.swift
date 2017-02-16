ed1ec64e07f5ce7cc32a850fa6eba8080bd027fe
Tim Kientzle <tkientzle@apple.com>
2017-02-16 12:32:11 -0800

    Issue #222: Duration range truncation
    `String(format:)` treats `%d` as a 32-bit integer, truncating
    the 64-bit seconds value used in `Google_Protobuf_Duration`.
    Change to `%ld` to preserve the 64-bit value.

--- Sources/SwiftProtobuf/Google_Protobuf_Duration_Extensions.swift
+++ Sources/SwiftProtobuf/Google_Protobuf_Duration_Extensions.swift
@@ -99,13 +99,13 @@ private func formatDuration(seconds: Int64, nanos: Int32) -> String? {
     }
 
     if nanos == 0 {
-        return String(format: "%ds", seconds)
+        return String(format: "%lds", seconds)
     } else if nanos % 1000000 == 0 {
-        return String(format: "%d.%03ds", seconds, abs(nanos) / 1000000)
+        return String(format: "%ld.%03ds", seconds, abs(nanos) / 1000000)
     } else if nanos % 1000 == 0 {
-        return String(format: "%d.%06ds", seconds, abs(nanos) / 1000)
+        return String(format: "%ld.%06ds", seconds, abs(nanos) / 1000)
     } else {
-        return String(format: "%d.%09ds", seconds, abs(nanos))
+        return String(format: "%ld.%09ds", seconds, abs(nanos))
     }
 }
 
