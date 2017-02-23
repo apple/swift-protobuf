ef73d89d2270aaa2573f238f4821bf0192a9713f
Tony Allevato <allevato@google.com>
2017-02-23 09:50:49 -0800

    Delete ExtensibleMessageStorage.

--- Sources/SwiftProtobuf/ExtensibleMessage.swift
+++ Sources/SwiftProtobuf/ExtensibleMessage.swift
@@ -19,35 +19,3 @@ public protocol ExtensibleMessage: Message {
     func hasExtensionValue<F: AnyExtensionField>(ext: MessageExtension<F, Self>) -> Bool
     mutating func clearExtensionValue<F: AnyExtensionField>(ext: MessageExtension<F, Self>)
 }
-
-
-// -----------------------------------------------------------------------------
-
-
-/// SwiftProtobuf Internal: Common support for storage classes to handle extension fields
-public protocol _ExtensibleMessageStorage: class {
-  associatedtype ExtendedMessage: Message
-  var extensionFieldValues: ExtensionFieldValueSet {get set}
-}
-
-/// SwiftProtobuf Internal: Common support for storage classes to handle extension fields
-public extension _ExtensibleMessageStorage {
-  public func setExtensionValue<F: AnyExtensionField>(ext: MessageExtension<F, ExtendedMessage>, value: F.ValueType) {
-    extensionFieldValues[ext.fieldNumber] = ext.set(value: value)
-  }
-
-  public func clearExtensionValue<F: AnyExtensionField>(ext: MessageExtension<F, ExtendedMessage>) {
-    extensionFieldValues[ext.fieldNumber] = nil
-  }
-
-  public func getExtensionValue<F: AnyExtensionField>(ext: MessageExtension<F, ExtendedMessage>) -> F.ValueType {
-    if let fieldValue = extensionFieldValues[ext.fieldNumber] as? F {
-      return fieldValue.value
-    }
-    return ext.defaultValue
-  }
-
-  public func hasExtensionValue<F: AnyExtensionField>(ext: MessageExtension<F, ExtendedMessage>) -> Bool {
-    return extensionFieldValues[ext.fieldNumber] is F
-  }
-}
