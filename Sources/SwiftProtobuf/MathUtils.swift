a7a10cf1136d48101fbcb4abc081e7831d6387a0
Tim Kientzle <tkientzle@apple.com>
2017-03-08 13:02:56 -0700

    Swift on Linux does not expose the C FLT_DIG and DBL_DIG constants by default.
    In theory, we should be able to derive these from
    the number of significand bits, which Swift does
    uniformly expose on all platforms.

--- Sources/SwiftProtobuf/MathUtils.swift
+++ Sources/SwiftProtobuf/MathUtils.swift
@@ -12,6 +12,12 @@
 ///
 // -----------------------------------------------------------------------------
 
+#if os(Linux)
+// Linux doesn't seem to define these by default.
+internal let FLT_DIG=6
+internal let DBL_DIG=15
+#endif
+
 /// Remainder in standard modular arithmetic (modulo). This coincides with (%)
 /// when a > 0.
 ///
