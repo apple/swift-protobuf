3efadc5156ca7572a50e19a6e973c096c1c38757
Tim Kientzle <tkientzle@apple.com>
2017-03-10 11:29:01 -0800

    Attempt to correct the handling of zero bytes in UTF8 strings on Linux

--- Sources/SwiftProtobuf/StringUtils.swift
+++ Sources/SwiftProtobuf/StringUtils.swift
@@ -17,7 +17,40 @@ import Foundation
 // Convert a pointer/count that refers to a block of UTF8 bytes
 // in memory into a String.
 // Returns nil if UTF8 is invalid.
+
+// This is painfully slow but seems to work correctly on every platform.
+#if os(Linux)
+fileprivate func slowUtf8ToString(bytes: UnsafePointer<UInt8>, count: Int) -> String? {
+    var s = ""
+    let buffer = UnsafeBufferPointer<UInt8>(start: bytes, count: count)
+    var bytes = buffer.makeIterator()
+    var utf8Decoder = UTF8()
+    while true {
+        switch utf8Decoder.decode(&bytes) {
+        case .scalarValue(let scalar): s.append(String(scalar))
+        case .emptyInput: return s
+        case .error: return nil
+        }
+    }
+}
+#endif
+
 internal func utf8ToString(bytes: UnsafePointer<UInt8>, count: Int) -> String? {
+    if count == 0 {
+        return ""
+    }
+#if os(Linux)
+    // As of March, 2017, the NSString(bytes:length:encoding:)
+    // initializer incorrectly stops at the first zero character for
+    // Linux versions of Swift.  So test for the presence of a zero byte
+    // and fall back to a slow-but-correct conversion in that case:
+    if count > Int(Int32.max) {
+        return nil
+    }
+    if let zeroLocation = memchr(bytes, Int32(truncatingBitPattern: count), 0) {
+        return slowUtf8ToString(bytes: bytes, count: count)
+    }
+#endif
     let s = NSString(bytes: bytes, length: count, encoding: String.Encoding.utf8.rawValue)
     if let s = s {
         return String._unconditionallyBridgeFromObjectiveC(s)
