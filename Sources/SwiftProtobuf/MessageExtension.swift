21321e802fbf92db2946a149fde92d2ce4b25db7
Tim Kientzle <tkientzle@apple.com>
2017-03-20 14:13:34 -0700

    Make `fieldName` be a simple String property
    Pros:  Uses standard Swift types, allows us to
       compute the fieldName using the surrounding scope
    
    Cons:  Performance hit on Text format serialization,
       Strings require more data space than StaticString

--- Sources/SwiftProtobuf/MessageExtension.swift
+++ Sources/SwiftProtobuf/MessageExtension.swift
@@ -19,7 +19,7 @@
 /// pieces.
 public protocol MessageExtensionBase {
     var fieldNumber: Int { get }
-    var fieldName: StaticString { get }
+    var fieldName: String { get }
     var messageType: Message.Type { get }
     func _protobuf_newField() -> AnyExtensionField
 }
@@ -29,10 +29,10 @@ public protocol MessageExtensionBase {
 /// compile-time compatibility checks.
 public class MessageExtension<FieldType: ExtensionField, MessageType: Message>: MessageExtensionBase {
     public let fieldNumber: Int
-    public let fieldName: StaticString
+    public let fieldName: String
     public let messageType: Message.Type
     public let defaultValue: FieldType.ValueType
-    public init(_protobuf_fieldNumber: Int, fieldName: StaticString, defaultValue: FieldType.ValueType) {
+    public init(_protobuf_fieldNumber: Int, fieldName: String, defaultValue: FieldType.ValueType) {
         self.fieldNumber = _protobuf_fieldNumber
         self.fieldName = fieldName
         self.messageType = MessageType.self
