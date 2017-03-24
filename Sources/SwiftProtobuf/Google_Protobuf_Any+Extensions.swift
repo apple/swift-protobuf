fbda3fb7b70f714ab2a324af41bda0ca2a773456
Tony Allevato <allevato@google.com>
2017-03-23 20:25:29 -0700

    Move Message extension for Any into its own file.

--- Sources/SwiftProtobuf/Google_Protobuf_Any+Extensions.swift
+++ Sources/SwiftProtobuf/Google_Protobuf_Any+Extensions.swift
@@ -8,40 +8,13 @@
 //
 // -----------------------------------------------------------------------------
 ///
-/// Extends the Google_Protobuf_Any and Message structs with various
-/// custom behaviors.
+/// Extends the `Google_Protobuf_Any` type with various custom behaviors.
 ///
 // -----------------------------------------------------------------------------
 
 import Foundation
 
-public extension Message {
-  /// Initialize this message from the provided `google.protobuf.Any`
-  /// well-known type.
-  ///
-  /// This corresponds to the `unpack` method in the Google C++ API.
-  ///
-  /// If the Any object was decoded from Protobuf Binary or JSON
-  /// format, then the enclosed field data was stored and is not
-  /// fully decoded until you unpack the Any object into a message.
-  /// As such, this method will typically need to perform a full
-  /// deserialization of the enclosed data and can fail for any
-  /// reason that deserialization can fail.
-  ///
-  /// See `Google_Protobuf_Any.unpackTo()` for more discussion.
-  ///
-  /// - Parameter unpackingAny: the message to decode.
-  /// - Throws: an instance of `AnyUnpackError`, `JSONDecodingError`, or
-  ///   `BinaryDecodingError` on failure.
-  public init(unpackingAny: Google_Protobuf_Any, extensions: ExtensionMap? = nil) throws {
-    self.init()
-    try unpackingAny._storage.unpackTo(target: &self, extensions: extensions)
-  }
-}
-
-
 public extension Google_Protobuf_Any {
-
   /// Initialize an Any object from the provided message.
   ///
   /// This corresponds to the `pack` operation in the C++ API.
