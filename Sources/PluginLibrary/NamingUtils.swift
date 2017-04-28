57b0552346193ef7c5d8fcd162795db2b0193a44
Thomas Van Lenten <thomasvl@google.com>
2017-04-28 14:45:43 -0400

    Move the ReservedWords helpers into NamingUtils.
    This will let Descriptor use them.
    Add some tests for them.

--- Sources/PluginLibrary/NamingUtils.swift
+++ Sources/PluginLibrary/NamingUtils.swift
@@ -10,8 +10,183 @@
 ///
 /// This provides some utilities for generating names.
 ///
+/// NOTE: These are *NOT* public since the expectation is the results exposed
+/// on the Descriptor object is all that is needed.
+///
 // -----------------------------------------------------------------------------
 
+///
+/// We won't generate types (structs, enums) with these names:
+///
+private let reservedTypeNames: Set<String> = {
+  () -> Set<String> in
+
+  var names: Set<String> = []
+
+  // Main SwiftProtobuf namespace
+  // Shadowing this leads to Bad Things.
+  names.insert("SwiftProtobuf")
+
+  // Subtype of many messages, used to scope nested extensions
+  names.insert("Extensions")
+
+  // Subtypes are static references, so can conflict with static
+  // class properties:
+  names.insert("protoMessageName")
+
+  // Methods on Message that we need to avoid shadowing.  Testing
+  // shows we do not need to avoid `serializedData` or `isEqualTo`,
+  // but it's not obvious to me what's different about them.  Maybe
+  // because these two are generic?  Because they throw?
+  names.insert("decodeMessage")
+  names.insert("traverse")
+
+  // Basic Message properties we don't want to shadow:
+  names.insert("isInitialized")
+  names.insert("unknownFields")
+
+  // Standard Swift property names we don't want
+  // to conflict with:
+  names.insert("debugDescription")
+  names.insert("description")
+  names.insert("dynamicType")
+  names.insert("hashValue")
+
+  // We don't need to protect all of these keywords, just the ones
+  // that interfere with type expressions:
+  // names = names.union(swiftKeywordsReservedInParticularContexts)
+  names.insert("Type")
+  names.insert("Protocol")
+
+  names = names.union(swiftKeywordsUsedInDeclarations)
+  names = names.union(swiftKeywordsUsedInStatements)
+  names = names.union(swiftKeywordsUsedInExpressionsAndTypes)
+  names = names.union(swiftCommonTypes)
+  names = names.union(swiftSpecialVariables)
+  return names
+}()
+
+private let reservedFieldNames: Set<String> = {
+  () -> Set<String> in
+  var names: Set<String> = []
+
+  // Properties are instance names, so can't shadow static class
+  // properties such as `protoMessageName`.
+
+  // Properties can't shadow methods.  For example, we don't need to
+  // avoid `isEqualTo` as a field name.
+
+  // Basic Message properties that we don't want to shadow
+  names.insert("isInitialized")
+  names.insert("unknownFields")
+
+  // Standard Swift property names we don't want
+  // to conflict with:
+  names.insert("debugDescription")
+  names.insert("description")
+  names.insert("dynamicType")
+  names.insert("hashValue")
+
+  // We don't need to protect all of these keywords, just the ones
+  // that interfere with type expressions:
+  // names = names.union(swiftKeywordsReservedInParticularContexts)
+  names.insert("Type")
+  names.insert("Protocol")
+
+  names = names.union(swiftKeywordsUsedInDeclarations)
+  names = names.union(swiftKeywordsUsedInStatements)
+  names = names.union(swiftKeywordsUsedInExpressionsAndTypes)
+  names = names.union(swiftCommonTypes)
+  names = names.union(swiftSpecialVariables)
+  return names
+}()
+
+/*
+ * Many Swift reserved words can be used as enum cases if we put
+ * backticks around them:
+ */
+private let quotableEnumCases: Set<String> = {
+  () -> Set<String> in
+  var names: Set<String> = []
+
+  // We don't need to protect all of these keywords, just the ones
+  // that interfere with enum cases:
+  // names = names.union(swiftKeywordsReservedInParticularContexts)
+  names.insert("associativity")
+  names.insert("dynamicType")
+  names.insert("optional")
+  names.insert("required")
+
+  names = names.union(swiftKeywordsUsedInDeclarations)
+  names = names.union(swiftKeywordsUsedInStatements)
+  names = names.union(swiftKeywordsUsedInExpressionsAndTypes)
+  // Common type and variable names don't cause problems as enum
+  // cases, because enum case names only appear in special contexts:
+  // names = names.union(swiftCommonTypes)
+  // names = names.union(swiftSpecialVariables)
+  return names
+}()
+
+/*
+ * Some words cannot be used for enum cases, even if they
+ * are quoted with backticks:
+ */
+private let reservedEnumCases: Set<String> = [
+  // Don't conflict with standard Swift property names:
+  "debugDescription",
+  "description",
+  "dynamicType",
+  "hashValue",
+  "init",
+  "rawValue",
+  "self",
+]
+
+
+private func isAllUnderscore(_ s: String) -> Bool {
+  if s.isEmpty {
+    return false
+  }
+  for c in s.characters {
+    if c != "_" {return false}
+  }
+  return true
+}
+
+
+private func sanitizeTypeName(_ s: String, disambiguator: String) -> String {
+  if reservedTypeNames.contains(s) {
+    return s + disambiguator
+  } else if isAllUnderscore(s) {
+    return s + disambiguator
+  } else if s.hasSuffix(disambiguator) {
+    // If `foo` and `fooMessage` both exist, and `foo` gets
+    // expanded to `fooMessage`, then we also should expand
+    // `fooMessage` to `fooMessageMessage` to avoid creating a new
+    // conflict.  This can be resolved recursively by stripping
+    // the disambiguator, sanitizing the root, then re-adding the
+    // disambiguator:
+    let e = s.index(s.endIndex, offsetBy: -disambiguator.characters.count)
+    let truncated = s.substring(to: e)
+    return sanitizeTypeName(truncated, disambiguator: disambiguator) + disambiguator
+  } else {
+    return s
+  }
+}
+
+
+/*
+ * Many Swift reserved words can be used as Extension names if we put
+ * backticks around them.
+ *
+ * Note: To avoid the duplicate list to maintain, currently just reusing the
+ *       EnumCases one.
+ */
+private let quotableMessageScopedExtensionNames: Set<String> = quotableEnumCases
+
+
+// Scope for the utilies to they are less likely to conflict when imported into
+// generators.
 enum NamingUtils {
 
   // Returns the type prefix to use for a given
@@ -46,6 +221,69 @@ enum NamingUtils {
     }
     // End in an underscore to split off anything that gets added to it.
     return prefix + "_"
+  }
+
+  static func sanitize(messageName s: String) -> String {
+    return sanitizeTypeName(s, disambiguator: "Message")
+  }
+
+  static func sanitize(enumName s: String) -> String {
+    return sanitizeTypeName(s, disambiguator: "Enum")
+  }
+
+  static func sanitize(oneofName s: String) -> String {
+    return sanitizeTypeName(s, disambiguator: "Oneof")
+  }
+
+  /// Struct and class field names go through
+  /// this before going into the source code.
+  /// It appends "_p" to any name that can't be
+  /// used as a field name in Swift source code.
+  static func sanitize(fieldName s: String, basedOn: String) -> String {
+    if basedOn.hasPrefix("clear") {
+      return s + "_p"
+    } else if basedOn.hasPrefix("has") {
+      return s + "_p"
+    } else if reservedFieldNames.contains(basedOn) {
+      return s + "_p"
+    } else if isAllUnderscore(basedOn) {
+      return s + "__"
+    } else {
+      return s
+    }
+  }
+
+  static func sanitize(fieldName s: String) -> String {
+    return sanitize(fieldName: s, basedOn: s)
+  }
+
+  /// enum case names are sanitized by adding
+  /// backticks `` around them.
+  static func sanitize(enumCaseName s: String) -> String {
+    if reservedEnumCases.contains(s) {
+      return "\(s)_"
+    } else if quotableEnumCases.contains(s) {
+      return "`\(s)`"
+    } else if isAllUnderscore(s) {
+      return s + "__"
+    } else {
+      return s
+    }
+  }
+
+  static func sanitize(messageScopedExtensionName s: String, skipBackticks: Bool = false) -> String {
+    // Since thing else is added to the "enum Extensions" for scoped
+    // extensions, there is no need to have a reserved list.
+    if quotableMessageScopedExtensionNames.contains(s) {
+      if skipBackticks {
+        return s
+      }
+      return "`\(s)`"
+    } else if isAllUnderscore(s) {
+      return s + "__"
+    } else {
+      return s
+    }
   }
 
 }
--- Tests/PluginLibraryTests/Test_NamingUtils.swift
+++ Tests/PluginLibraryTests/Test_NamingUtils.swift
@@ -48,4 +48,200 @@ class Test_NamingUtils: XCTestCase {
       XCTAssertEqual(result, expected, "Package: \(package), Prefix: \(prefix ?? "nil")")
     }
   }
+
+  func testSanitize_messageName() {
+    // input, expected
+    let tests: [(String, String)] = [
+      ( "", "" ),
+
+      ( "Foo", "Foo" ),
+      ( "FooBar", "FooBar" ),
+      ( "foo_bar", "foo_bar" ),
+
+      // Some of our names get the disambiguator added.
+      ( "SwiftProtobuf", "SwiftProtobufMessage" ),
+      ( "isInitialized", "isInitializedMessage" ),
+
+      // Some Swift keywords.
+      ( "associatedtype", "associatedtypeMessage" ),
+      ( "class", "classMessage" ),
+      ( "break", "breakMessage" ),
+      ( "do", "doMessage" ),
+
+      // Inputs with the disambiguator.
+      ( "classMessage", "classMessageMessage" ),
+      ( "classMessageMessage", "classMessageMessageMessage" ),
+
+      // Underscores
+      ( "_", "_Message" ),
+      ( "___", "___Message" ),
+    ]
+    for (input, expected) in tests {
+      XCTAssertEqual(NamingUtils.sanitize(messageName: input), expected)
+    }
+  }
+
+  func testSanitize_enumName() {
+    // input, expected
+    let tests: [(String, String)] = [
+      ( "", "" ),
+
+      ( "Foo", "Foo" ),
+      ( "FooBar", "FooBar" ),
+      ( "foo_bar", "foo_bar" ),
+
+      // Some of our names get the disambiguator added.
+      ( "SwiftProtobuf", "SwiftProtobufEnum" ),
+      ( "isInitialized", "isInitializedEnum" ),
+
+      // Some Swift keywords.
+      ( "associatedtype", "associatedtypeEnum" ),
+      ( "class", "classEnum" ),
+      ( "break", "breakEnum" ),
+      ( "do", "doEnum" ),
+
+      // Inputs with the disambiguator.
+      ( "classEnum", "classEnumEnum" ),
+      ( "classEnumEnum", "classEnumEnumEnum" ),
+
+      // Underscores
+      ( "_", "_Enum" ),
+      ( "___", "___Enum" ),
+    ]
+    for (input, expected) in tests {
+      XCTAssertEqual(NamingUtils.sanitize(enumName: input), expected)
+    }
+  }
+
+  func testSanitize_oneofName() {
+    // input, expected
+    let tests: [(String, String)] = [
+      ( "", "" ),
+
+      ( "Foo", "Foo" ),
+      ( "FooBar", "FooBar" ),
+      ( "foo_bar", "foo_bar" ),
+
+      // Some of our names get the disambiguator added.
+      ( "SwiftProtobuf", "SwiftProtobufOneof" ),
+      ( "isInitialized", "isInitializedOneof" ),
+
+      // Some Swift keywords.
+      ( "associatedtype", "associatedtypeOneof" ),
+      ( "class", "classOneof" ),
+      ( "break", "breakOneof" ),
+      ( "do", "doOneof" ),
+
+      // Inputs with the disambiguator.
+      ( "classOneof", "classOneofOneof" ),
+      ( "classOneofOneof", "classOneofOneofOneof" ),
+
+      // Underscores
+      ( "_", "_Oneof" ),
+      ( "___", "___Oneof" ),
+    ]
+    for (input, expected) in tests {
+      XCTAssertEqual(NamingUtils.sanitize(oneofName: input), expected)
+    }
+  }
+
+  func testSanitize_fieldName() {
+    // input, expected
+    let tests: [(String, String)] = [
+      ( "", "" ),
+
+      ( "Foo", "Foo" ),
+      ( "FooBar", "FooBar" ),
+      ( "foo_bar", "foo_bar" ),
+
+      // Some of our names get the disambiguator added.
+      ( "debugDescription", "debugDescription_p" ),
+      ( "isInitialized", "isInitialized_p" ),
+
+      // Some Swift keywords.
+      ( "associatedtype", "associatedtype_p" ),
+      ( "class", "class_p" ),
+      ( "break", "break_p" ),
+      ( "do", "do_p" ),
+
+      // "has"/"clear" get added by us, so they get the disambiguator.
+      ( "hasFoo", "hasFoo_p" ),
+      ( "clearFoo", "clearFoo_p" ),
+
+      // Underscores get more underscores.
+      ( "_", "___" ),
+      ( "___", "_____" ),
+    ]
+
+    func uppercaseFirst(_ s: String) -> String {
+      var result = s.characters
+      if let first = result.popFirst() {
+        return String(first).uppercased() + String(result)
+      } else {
+        return s
+      }
+    }
+
+    for (input, expected) in tests {
+      XCTAssertEqual(NamingUtils.sanitize(fieldName: input), expected)
+
+      let inputPrefixed = "XX" + uppercaseFirst(input)
+      let expected2 = "XX" + uppercaseFirst(expected)
+      XCTAssertEqual(NamingUtils.sanitize(fieldName: inputPrefixed, basedOn: input), expected2)
+    }
+  }
+
+  func testSanitize_enumCaseName() {
+    // input, expected
+    let tests: [(String, String)] = [
+      ( "", "" ),
+
+      ( "Foo", "Foo" ),
+      ( "FooBar", "FooBar" ),
+      ( "foo_bar", "foo_bar" ),
+
+      // Some of our names get the disambiguator added.
+      ( "debugDescription", "debugDescription_" ),
+      ( "dynamicType", "dynamicType_" ),
+
+      // Some Swift keywords work with backticks
+      ( "associatedtype", "`associatedtype`" ),
+      ( "class", "`class`" ),
+      ( "break", "`break`" ),
+      ( "do", "`do`" ),
+
+      // Underscores get more underscores.
+      ( "_", "___" ),
+      ( "___", "_____" ),
+    ]
+
+    for (input, expected) in tests {
+      XCTAssertEqual(NamingUtils.sanitize(enumCaseName: input), expected)
+    }
+  }
+  
+  func testSanitize_messageScopedExtensionName() {
+    // input, expected
+    let tests: [(String, String)] = [
+      ( "", "" ),
+
+      ( "Foo", "Foo" ),
+      ( "FooBar", "FooBar" ),
+      ( "foo_bar", "foo_bar" ),
+
+      // Some Swift keywords work with backticks
+      ( "associatedtype", "`associatedtype`" ),
+      ( "class", "`class`" ),
+      ( "break", "`break`" ),
+      ( "do", "`do`" ),
+
+      // Underscores get more underscores.
+      ( "_", "___" ),
+      ( "___", "_____" ),
+    ]
+
+    for (input, expected) in tests {
+      XCTAssertEqual(NamingUtils.sanitize(messageScopedExtensionName: input), expected)
+    }
+  }
 }
