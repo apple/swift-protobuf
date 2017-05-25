fec2628a98ebd6f3aabcf15401781f393483c458
Thomas Van Lenten <thomasvl@google.com>
2017-05-25 13:58:28 -0400

    Tighten up the bool TextFormat parsing.
    During TextFormat parsing, if the field was boolean, only the first
    character was used if it was "0" or "1", not the next whole token.
    This let bad input either choke some what by accident later or
    could allow things to slip through as valid when no other text
    parser could likely parse it.

--- Sources/SwiftProtobuf/TextFormatScanner.swift
+++ Sources/SwiftProtobuf/TextFormatScanner.swift
@@ -57,6 +57,7 @@ private let asciiLowerI = UInt8(ascii: "i")
 private let asciiLowerN = UInt8(ascii: "n")
 private let asciiLowerR = UInt8(ascii: "r")
 private let asciiLowerT = UInt8(ascii: "t")
+private let asciiUpperT = UInt8(ascii: "T")
 private let asciiLowerU = UInt8(ascii: "u")
 private let asciiLowerV = UInt8(ascii: "v")
 private let asciiLowerX = UInt8(ascii: "x")
@@ -808,23 +809,19 @@ internal struct TextFormatScanner {
         }
         let c = p[0]
         switch c {
-        case asciiZero: // 0
-            p += 1
-            return false
-        case asciiOne: // 1
-            p += 1
-            return true
-        default:
+        case asciiZero, asciiOne, asciiLowerF, asciiUpperF, asciiLowerT, asciiUpperT:
             if let s = parseIdentifier() {
                 switch s {
-                case "f", "false", "False":
+                case "0", "f", "false", "False":
                     return false
-                case "t", "true", "True":
+                case "1", "t", "true", "True":
                     return true
                 default:
                     break
                 }
             }
+        default:
+            break
         }
         throw TextFormatDecodingError.malformedText
     }
