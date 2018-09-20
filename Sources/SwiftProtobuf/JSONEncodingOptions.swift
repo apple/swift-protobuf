0de27ae03709e5646b64abd9fa0b43de8fe82ec7
Thomas Van Lenten <thomasvl@google.com>
2018-09-20 13:57:00 -0400

    Add JSONEncodingOptions.swift

---
+++ Sources/SwiftProtobuf/JSONEncodingOptions.swift
@@ -0,0 +1,19 @@
+// Sources/SwiftProtobuf/JSONEncodingOptions.swift - JSON encoding options
+//
+// Copyright (c) 2014 - 2018 Apple Inc. and the project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See LICENSE.txt for license information:
+// https://github.com/apple/swift-protobuf/blob/master/LICENSE.txt
+//
+// -----------------------------------------------------------------------------
+///
+/// JSON encoding options
+///
+// -----------------------------------------------------------------------------
+
+/// Options for JSONEncoding.
+public struct JSONEncodingOptions {
+
+  public init() {}
+}
