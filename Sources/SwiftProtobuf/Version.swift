6520fb185db88c0774a929acea1f7d5981a30d3a
Thomas Van Lenten <thomasvl@google.com>
2019-03-11 13:01:01 -0400

    Bump the version to 1.4.0 to cut a release.

--- Sources/SwiftProtobuf/Version.swift
+++ Sources/SwiftProtobuf/Version.swift
@@ -19,9 +19,9 @@ public struct Version {
   /// Major version.
   public static let major = 1
   /// Minor version.
-  public static let minor = 3
+  public static let minor = 4
   /// Revision number.
-  public static let revision = 1
+  public static let revision = 0
 
   /// String form of the version number.
   public static let versionString = "\(major).\(minor).\(revision)"
