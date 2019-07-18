bdf8a74e3797a56503194d8b7874c83d8e68dbc6
Thomas Van Lenten <thomasvl@google.com>
2019-07-18 08:21:52 -0400

    Bump the version to 1.6 for a release.

--- Sources/SwiftProtobuf/Version.swift
+++ Sources/SwiftProtobuf/Version.swift
@@ -19,7 +19,7 @@ public struct Version {
   /// Major version.
   public static let major = 1
   /// Minor version.
-  public static let minor = 5
+  public static let minor = 6
   /// Revision number.
   public static let revision = 0
 
