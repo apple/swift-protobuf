0803e98df1eb9a0f9d91deff08ff66e10d753c12
Thomas Van Lenten <thomasvl@google.com>
2020-08-11 16:50:06 -0400

    Regenerate.

--- Reference/unittest_swift_oneof_all_required.pb.swift
+++ Reference/unittest_swift_oneof_all_required.pb.swift
@@ -140,10 +140,22 @@ struct ProtobufUnittest_OneOfContainer {
     case option4(Int32)
 
     fileprivate var isInitialized: Bool {
+      // The use of inline closures is to circumvent an issue where the compiler
+      // allocates stack space for every case branch when no optimizations are
+      // enabled. https://github.com/apple/swift-protobuf/issues/1034
       switch self {
-      case .option1(let v): return v.isInitialized
-      case .option2(let v): return v.isInitialized
-      case .option3(let v): return v.isInitialized
+      case .option1: return {
+        guard case .option1(let v) = self else { preconditionFailure() }
+        return v.isInitialized
+      }()
+      case .option2: return {
+        guard case .option2(let v) = self else { preconditionFailure() }
+        return v.isInitialized
+      }()
+      case .option3: return {
+        guard case .option3(let v) = self else { preconditionFailure() }
+        return v.isInitialized
+      }()
       default: return true
       }
     }
