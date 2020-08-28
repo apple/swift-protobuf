0279688c9fc5a40028e1b5bb0cb56534a45a6020
Thomas Van Lenten <thomasvl@google.com>
2020-08-28 09:33:33 -0400

    Version bump for a release.

--- Sources/SwiftProtobuf/Version.swift
+++ Sources/SwiftProtobuf/Version.swift
@@ -19,7 +19,7 @@ public struct Version {
   /// Major version.
   public static let major = 1
   /// Minor version.
-  public static let minor = 11
+  public static let minor = 12
   /// Revision number.
   public static let revision = 0
 
