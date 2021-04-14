afe828b40394f68482167a14cc4d17fabb4c357c
Thomas Van Lenten <thomasvl@google.com>
2021-04-14 10:38:19 -0400

    Support building fuzz tests in debug or release configs.

--- FuzzTesting/do_build.sh
+++ FuzzTesting/do_build.sh
@@ -6,10 +6,47 @@ readonly FuzzTestingDir=$(dirname "$(echo $0 | sed -e "s,^\([^/]\),$(pwd)/\1,")"
 
 cd "${FuzzTestingDir}"
 
+# Default to both
+CMD_CONFIGS=("debug" "release")
+
+while [[ $# != 0 ]]; do
+  case "${1}" in
+    --debug )
+      CMD_CONFIGS=("debug")
+      ;;
+    --release )
+      CMD_CONFIGS=("release")
+      ;;
+    --both )
+      CMD_CONFIGS=("debug" "release")
+      ;;
+    -*)
+      echo "ERROR: Unknown option: ${1}" 1>&2
+      exit 1
+      ;;
+    *)
+      echo "ERROR: Unknown argument: ${1}" 1>&2
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+declare -a CMD_BASE
 if [ "$(uname)" == "Darwin" ]; then
-  xcrun \
-    --toolchain swift \
-    swift build -c debug -Xswiftc -sanitize=fuzzer,address -Xswiftc -parse-as-library
+  CMD_BASE=(
+    xcrun
+      --toolchain swift
+      swift build -Xswiftc -sanitize=fuzzer,address -Xswiftc -parse-as-library
+  )
 else
-  swift build -c debug -Xswiftc -sanitize=fuzzer,address -Xswiftc -parse-as-library
+  CMD_BASE=(
+    swift build -Xswiftc -sanitize=fuzzer,address -Xswiftc -parse-as-library
+  )
 fi
+
+for CMD_CONFIG in "${CMD_CONFIGS[@]}"; do
+  echo "Building: ${CMD_CONFIG}"
+  echo "${CMD_BASE[@]}" -c "${CMD_CONFIG}"
+  "${CMD_BASE[@]}" -c "${CMD_CONFIG}"
+done
