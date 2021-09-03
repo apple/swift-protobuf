a9faaefed737c95c324daaa8304e2a45935e20d8
Thomas Van Lenten <thomasvl@google.com>
2021-09-03 13:04:41 -0400

    Serialize after decoding/parsing.
    Looking at the fuzz coverage
      https://storage.googleapis.com/oss-fuzz-coverage/swift-protobuf/reports/20210903/linux/report.html
    One thing that sticks out is nothing testing serialization, so add serialization
    when a message was parsed just to ensure things are safe there also.

--- FuzzTesting/Sources/FuzzJSON/main.swift
+++ FuzzTesting/Sources/FuzzJSON/main.swift
@@ -5,13 +5,16 @@ import FuzzCommon
 @_cdecl("LLVMFuzzerTestOneInput")
 public func FuzzJSON(_ start: UnsafeRawPointer, _ count: Int) -> CInt {
   let bytes = UnsafeRawBufferPointer(start: start, count: count)
+  var msg: Fuzz_Testing_Message?
   do {
-    let _ = try Fuzz_Testing_Message(
+    msg = try Fuzz_Testing_Message(
       jsonUTF8Data: Data(bytes),
       extensions: Fuzz_Testing_FuzzTesting_Extensions)
   } catch {
     // Error parsing are to be expected since not all input will be well formed.
   }
-
+  // Test serialization for completeness.
+  // If a message was parsed, it should not fail to serialize, so assert as such.
+  let _ = try! msg?.jsonString()
   return 0
 }
--- FuzzTesting/Sources/FuzzTextFormat/main.swift
+++ FuzzTesting/Sources/FuzzTextFormat/main.swift
@@ -6,13 +6,16 @@ import FuzzCommon
 public func FuzzTextFormat(_ start: UnsafeRawPointer, _ count: Int) -> CInt {
   let bytes = UnsafeRawBufferPointer(start: start, count: count)
   guard let str = String(data: Data(bytes), encoding: .utf8) else { return 0 }
+  var msg: Fuzz_Testing_Message?
   do {
-    let _ = try Fuzz_Testing_Message(
+    msg = try Fuzz_Testing_Message(
       textFormatString: str,
       extensions: Fuzz_Testing_FuzzTesting_Extensions)
   } catch {
     // Error parsing are to be expected since not all input will be well formed.
   }
+  // Test serialization for completeness.
+  let _ = msg?.textFormatString()
 
   return 0
 }
