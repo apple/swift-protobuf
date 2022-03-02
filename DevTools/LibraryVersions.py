3e6587f04366d737b221188f65efada896ea3dd3
Thomas Van Lenten <thomasvl@google.com>
2022-03-02 17:16:27 -0500

    Drop the Xcode project.
    Xcode has good support for SwiftPM at this point, so stop maintaining a project
    manually.

--- DevTools/LibraryVersions.py
+++ DevTools/LibraryVersions.py
@@ -20,7 +20,6 @@ _VERSION_RE = re.compile(r'^(?P<major>\d+)\.(?P<minor>\d+)(.(?P<revision>\d+))?$
 _PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 _PODSPEC_PATH = os.path.join(_PROJECT_ROOT, 'SwiftProtobuf.podspec')
 _VERSION_SWIFT_PATH = os.path.join(_PROJECT_ROOT, 'Sources/SwiftProtobuf/Version.swift')
-_XCCONFIG_PATH = os.path.join(_PROJECT_ROOT, 'SwiftProtobuf.xcodeproj/xcconfigs/Base.xcconfig')
 
 def Fail(message):
   sys.stderr.write('Error: %s\n' % message)
@@ -51,12 +50,6 @@ def ValidateFiles():
   if not had_major or not had_minor or not had_revision:
     Fail('Version in Sources/SwiftProtobuf/Version.swift did not match SwiftProtobuf.podspec')
 
-  # Test SwiftProtobuf.xcodeproj/xcconfigs/Base.xcconfig
-  xcconfig_content = open(_XCCONFIG_PATH).read()
-  current_version_line = 'CURRENT_PROJECT_VERSION = %s.%s.%s' % (major, minor, revision)
-  if current_version_line not in  xcconfig_content:
-    Fail('Version in SwiftProtobuf.xcodeproj/xcconfigs/Base.xcconfig did not match SwiftProtobuf.podspec')
-
 
 def UpdateFiles(version_string):
   (major, minor, revision) = ExtractVersion(version_string)
@@ -80,13 +73,6 @@ def UpdateFiles(version_string):
                                  'public static let revision = %s\n' % revision,
                                  version_swift_content)
   open(_VERSION_SWIFT_PATH, 'w').write(version_swift_content)
-
-  # Update SwiftProtobuf.xcodeproj/xcconfigs/Base.xcconfig
-  xcconfig_content = open(_XCCONFIG_PATH).read()
-  xcconfig_content = re.sub(r'CURRENT_PROJECT_VERSION = \d+\.\d+\.\d+',
-                            'CURRENT_PROJECT_VERSION = %s.%s.%s' % (major, minor, revision),
-                            xcconfig_content)
-  open(_XCCONFIG_PATH, 'w').write(xcconfig_content)
 
 
 def main(args):
