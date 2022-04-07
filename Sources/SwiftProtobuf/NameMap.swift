41410bc1ea9c0706cc9eb24f2bfc8793e14c8dba
Thomas Van Lenten <thomasvl@google.com>
2022-04-07 13:44:29 -0400

    Drop the Hashable support for < 4.2
    Progress on #1210

--- Sources/SwiftProtobuf/NameMap.swift
+++ Sources/SwiftProtobuf/NameMap.swift
@@ -67,12 +67,6 @@ fileprivate class InternPool {
   }
 }
 
-#if !swift(>=4.2)
-// Constants for FNV hash http://tools.ietf.org/html/draft-eastlake-fnv-03
-private let i_2166136261 = Int(bitPattern: 2166136261)
-private let i_16777619 = Int(16777619)
-#endif
-
 /// An immutable bidirectional mapping between field/enum-case names
 /// and numbers, used to record field names for text-based
 /// serialization (JSON and text).  These maps are lazily instantiated
@@ -135,21 +129,11 @@ public struct _NameMap: ExpressibleByDictionaryLiteral {
       }
     }
 
-  #if swift(>=4.2)
     public func hash(into hasher: inout Hasher) {
       for byte in utf8Buffer {
         hasher.combine(byte)
       }
     }
-  #else  // swift(>=4.2)
-    public var hashValue: Int {
-      var h = i_2166136261
-      for byte in utf8Buffer {
-        h = (h ^ Int(byte)) &* i_16777619
-      }
-      return h
-    }
-  #endif  // swift(>=4.2)
 
     public static func ==(lhs: Name, rhs: Name) -> Bool {
       if lhs.utf8Buffer.count != rhs.utf8Buffer.count {
