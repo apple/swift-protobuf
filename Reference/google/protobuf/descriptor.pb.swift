a6b946d2094c4b3f1393299c38c731c3868097a3
Thomas Van Lenten <thomasvl@google.com>
2022-10-10 09:31:22 -0400

    Regenerate current sources.

--- Reference/google/protobuf/descriptor.pb.swift
+++ Reference/google/protobuf/descriptor.pb.swift
@@ -1216,6 +1216,10 @@ struct Google_Protobuf_MessageOptions: SwiftProtobuf.ExtensibleMessage {
   /// Clears the value of `deprecated`. Subsequent reads from it will return its default value.
   mutating func clearDeprecated() {self._deprecated = nil}
 
+  /// NOTE: Do not set the option in .proto files. Always use the maps syntax
+  /// instead. The option should only be implicitly set by the proto compiler
+  /// parser.
+  ///
   /// Whether the message is an automatically generated map entry type for the
   /// maps field.
   ///
@@ -1233,10 +1237,6 @@ struct Google_Protobuf_MessageOptions: SwiftProtobuf.ExtensibleMessage {
   /// use a native map in the target language to hold the keys and values.
   /// The reflection APIs in such implementations still need to work as
   /// if the field is a repeated message field.
-  ///
-  /// NOTE: Do not set the option in .proto files. Always use the maps syntax
-  /// instead. The option should only be implicitly set by the proto compiler
-  /// parser.
   var mapEntry: Bool {
     get {return _mapEntry ?? false}
     set {_mapEntry = newValue}
