403a21d02b488b93fbdbcc1c4f1e7dbb5fbcfcf9
Gus Cairo <gus.cairo@apple.com>
2022-10-26 10:33:36 +0100

    Move Data conformance to protocol to separate file

--- Sources/SwiftProtobuf/SwiftProtobufContiguousBytes.swift
+++ Sources/SwiftProtobuf/SwiftProtobufContiguousBytes.swift
@@ -1,14 +1,12 @@
 // Sources/SwiftProtobuf/SwiftProtobufContiguousBytes.swift
 //
-// Copyright (c) 2014 - 2022 Apple Inc. and the project authors
+// Copyright (c) 2022 Apple Inc. and the project authors
 // Licensed under Apache License v2.0 with Runtime Library Exception
 //
 // See LICENSE.txt for license information:
 // https://github.com/apple/swift-protobuf/blob/main/LICENSE.txt
 //
 // -----------------------------------------------------------------------------
-
-import Foundation
 
 public protocol SwiftProtobufContiguousBytes {
     /// Calls the given closure with the contents of underlying storage.
@@ -21,5 +19,3 @@ public protocol SwiftProtobufContiguousBytes {
 }
 
 extension Array: SwiftProtobufContiguousBytes where Array.Element == UInt8 {}
-
-extension Data: SwiftProtobufContiguousBytes {}
