bc373517737ae674a5243b7f056d11ba0594ca19
Thomas Van Lenten <thomasvl@google.com>
2023-06-01 10:21:32 -0400

    Bump version for a 1.22.0 release.
    Mainly for the CocoaPods build issues.

--- Sources/SwiftProtobuf/Version.swift
+++ Sources/SwiftProtobuf/Version.swift
@@ -19,7 +19,7 @@ public struct Version {
   /// Major version.
   public static let major = 1
   /// Minor version.
-  public static let minor = 21
+  public static let minor = 22
   /// Revision number.
   public static let revision = 0
 
