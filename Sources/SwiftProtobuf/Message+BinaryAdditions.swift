5ae935c418e3087a788a6566bd883640113b9cea
Thomas Van Lenten <thomasvl@google.com>
2023-08-18 10:35:37 -0400

    Fix up some more docs errors.
    Catch more things Tony noticed when I tried to pull the previous fixes over to
    the 1.x branch.

--- Sources/SwiftProtobuf/Message+BinaryAdditions.swift
+++ Sources/SwiftProtobuf/Message+BinaryAdditions.swift
@@ -79,7 +79,7 @@ extension Message {
   ///     extensions in this message or messages nested within this message's
   ///     fields.
   ///   - partial: If `false` (the default), this method will check
-  ///     `Message.isInitialized` before decoding to verify that all required
+  ///     `Message.isInitialized` after decoding to verify that all required
   ///     fields are present. If any are missing, this method throws
   ///     `BinaryDecodingError.missingRequiredFields`.
   ///   - options: The BinaryDecodingOptions to use.
@@ -175,7 +175,7 @@ extension Message {
   ///     extensions in this message or messages nested within this message's
   ///     fields.
   ///   - partial: If `false` (the default), this method will check
-  ///     `Message.isInitialized` before decoding to verify that all required
+  ///     `Message.isInitialized` after decoding to verify that all required
   ///     fields are present. If any are missing, this method throws
   ///     `BinaryDecodingError.missingRequiredFields`.
   ///   - options: The BinaryDecodingOptions to use.
