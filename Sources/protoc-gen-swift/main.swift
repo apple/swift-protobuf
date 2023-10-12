744ab32df2fa33d82d046acf02f7e3e8328f8499
Thomas Van Lenten <thomasvl@google.com>
2023-10-12 15:43:02 -0400

    Migrate the plugin over to the builtin support now.

--- Sources/protoc-gen-swift/main.swift
+++ Sources/protoc-gen-swift/main.swift
@@ -24,171 +24,6 @@ import SwiftProtobuf
 import SwiftProtobufPluginLibrary
 
 struct GeneratorPlugin: CodeGenerator {
-  private enum Mode {
-    case showHelp
-    case showVersion
-    case generateFromStdin
-    case generateFromFiles(paths: [String])
-  }
-
-  func run(args: [String]) -> Int32 {
-    var result: Int32 = 0
-
-    let mode = parseCommandLine(args: args)
-    switch mode {
-    case .showHelp:
-      showHelp()
-    case .showVersion:
-      showVersion()
-    case .generateFromStdin:
-      result = generateFromStdin()
-    case .generateFromFiles(let paths):
-      result = generateFromFiles(paths)
-    }
-
-    return result
-  }
-
-  private func parseCommandLine(args: [String]) -> Mode {
-    var paths: [String] = []
-    for arg in args {
-      switch arg {
-      case "-h", "--help":
-        return .showHelp
-      case "--version":
-        return .showVersion
-      default:
-        if arg.hasPrefix("-") {
-          Stderr.print("Unknown argument: \"\(arg)\"")
-          return .showHelp
-        } else {
-          paths.append(arg)
-        }
-      }
-    }
-    return paths.isEmpty ? .generateFromStdin : .generateFromFiles(paths: paths)
-  }
-
-  private func showHelp() {
-    print("\(CommandLine.programName): Convert parsed proto definitions into Swift")
-    print("")
-    showVersion()
-    print(Version.copyright)
-    print("")
-
-    let version = SwiftProtobuf.Version.self
-    let packageVersion = "\(version.major),\(version.minor),\(version.revision)"
-
-    let help = (
-      "Note:  This is a plugin for protoc and should not normally be run\n"
-        + "directly.\n"
-        + "\n"
-        + "If you invoke a recent version of protoc with the --swift_out=<dir>\n"
-        + "option, then protoc will search the current PATH for protoc-gen-swift\n"
-        + "and use it to generate Swift output.\n"
-        + "\n"
-        + "In particular, if you have renamed this program, you will need to\n"
-        + "adjust the protoc command-line option accordingly.\n"
-        + "\n"
-        + "The generated Swift output requires the SwiftProtobuf \(SwiftProtobuf.Version.versionString)\n"
-        + "library be included in your project.\n"
-        + "\n"
-        + "If you use `swift build` to compile your project, add this to\n"
-        + "Package.swift:\n"
-        + "\n"
-        + "   dependencies: [\n"
-        + "     .package(name: \"SwiftProtobuf\", url: \"https://github.com/apple/swift-protobuf.git\", from: \"\(packageVersion)\"),"
-        + "   ]\n"
-        + "\n"
-        + "\n"
-        + "Usage: \(CommandLine.programName) [options] [filename...]\n"
-        + "\n"
-        + " -h|--help:  Print this help message\n"
-        + " --version: Print the program version\n"
-        + "\n"
-        + "Filenames specified on the command line indicate binary-encoded\n"
-        + "google.protobuf.compiler.CodeGeneratorRequest objects that will\n"
-        + "be read and converted to Swift source code.  The source text will be\n"
-        + "written directly to stdout.\n"
-        + "\n"
-        + "When invoked with no filenames, it will read a single binary-encoded\n"
-        + "google.protobuf.compiler.CodeGeneratorRequest object from stdin and\n"
-        + "emit the corresponding CodeGeneratorResponse object to stdout.\n")
-
-    print(help)
-  }
-
-  private func showVersion() {
-    print("\(CommandLine.programName) \(SwiftProtobuf.Version.versionString)")
-  }
-
-  private func generateFromStdin() -> Int32 {
-    let requestData = FileHandle.standardInput.readDataToEndOfFile()
-
-    // Support for loggin the request. Useful when protoc/protoc-gen-swift are
-    // being invoked from some build system/script. protoc-gen-swift supports
-    // loading a request as a command line argument to simplify debugging/etc.
-    if let dumpPath = ProcessInfo.processInfo.environment["PROTOC_GEN_SWIFT_LOG_REQUEST"], !dumpPath.isEmpty {
-      let dumpURL = URL(fileURLWithPath: dumpPath)
-      do {
-        try requestData.write(to: dumpURL)
-      } catch let e {
-        Stderr.print("Failed to write request to '\(dumpPath)', \(e)")
-      }
-    }
-
-    let request: Google_Protobuf_Compiler_CodeGeneratorRequest
-    do {
-      request = try Google_Protobuf_Compiler_CodeGeneratorRequest(serializedData: requestData)
-    } catch let e {
-      Stderr.print("Request failed to decode: \(e)")
-      return 1
-    }
-
-    auditProtoCVersion(request: request)
-    let response = generateCode(request: request, generator: self)
-    guard sendReply(response: response) else { return 1 }
-    return 0
-  }
-
-  private func generateFromFiles(_ paths: [String]) -> Int32 {
-    var result: Int32 = 0
-
-    for p in paths {
-      let requestData: Data
-      do {
-        requestData = try readFileData(filename: p)
-      } catch let e {
-        Stderr.print("Error reading from \(p) - \(e)")
-        result = 1
-        continue
-      }
-      Stderr.print("Read request: \(requestData.count) bytes from \(p)")
-
-      let request: Google_Protobuf_Compiler_CodeGeneratorRequest
-      do {
-        request = try Google_Protobuf_Compiler_CodeGeneratorRequest(serializedData: requestData)
-      } catch let e {
-        Stderr.print("Request failed to decode \(p): \(e)")
-        result = 1
-        continue
-      }
-
-      let response = generateCode(request: request, generator: self)
-      if response.hasError {
-        Stderr.print("Error while generating from \(p) - \(response.error)")
-        result = 1
-      } else {
-        for f in response.file {
-          print("+++ Begin File: \(f.name) +++")
-          print(!f.content.isEmpty ? f.content : "<No content>")
-          print("+++ End File: \(f.name) +++")
-        }
-      }
-    }
-
-    return result
-  }
 
   func generate(
     files: [SwiftProtobufPluginLibrary.FileDescriptor],
@@ -198,6 +33,7 @@ struct GeneratorPlugin: CodeGenerator {
   ) throws {
     let options = try GeneratorOptions(parameter: parameter)
 
+    auditProtoCVersion(context: protoCompilerContext)
     var errorString: String? = nil
     for fileDescriptor in files {
       let fileGenerator = FileGenerator(fileDescriptor: fileDescriptor, generatorOptions: options)
@@ -216,8 +52,12 @@ struct GeneratorPlugin: CodeGenerator {
     .proto3Optional,
   ]
 
-  private func auditProtoCVersion(request: Google_Protobuf_Compiler_CodeGeneratorRequest) {
-    guard request.hasCompilerVersion else {
+  var version: String? { return "\(SwiftProtobuf.Version.versionString)" }
+  var copyrightLine: String? { return "\(Version.copyright)" }
+  var projectURL: String? { return "https://github.com/apple/swift-protobuf" }
+
+  private func auditProtoCVersion(context: SwiftProtobufPluginLibrary.ProtoCompilerContext) {
+    guard context.version != nil else {
       Stderr.print("WARNING: unknown version of protoc, use 3.2.x or later to ensure JSON support is correct.")
       return
     }
@@ -226,24 +66,44 @@ struct GeneratorPlugin: CodeGenerator {
     // is there, the JSON support should be good.
   }
 
-  private func sendReply(response: Google_Protobuf_Compiler_CodeGeneratorResponse) -> Bool {
-    let serializedResponse: Data
-    do {
-      serializedResponse = try response.serializedData()
-    } catch let e {
-      Stderr.print("Failure while serializing response: \(e)")
-      return false
-    }
-    FileHandle.standardOutput.write(serializedResponse)
-    return true
+  // Provide an expanded version of help.
+  func printHelp() {
+    print("""
+      \(CommandLine.programName): Convert parsed proto definitions into Swift
+
+      \(Version.copyright)
+
+      Note:  This is a plugin for protoc and should not normally be run
+      directly.
+
+      If you invoke a recent version of protoc with the --swift_out=<dir>
+      option, then protoc will search the current PATH for protoc-gen-swift
+      and use it to generate Swift output.
+
+      In particular, if you have renamed this program, you will need to
+      adjust the protoc command-line option accordingly.
+
+      The generated Swift output requires the SwiftProtobuf \(version!)
+      library be included in your project.
+
+      If you use `swift build` to compile your project, add this to
+      Package.swift:
+
+         dependencies: [
+           .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", from: "\(version!)"),
+         ]
+
+      Usage: \(CommandLine.programName) [options] [filename...]
+
+        -h|--help:  Print this help message
+        --version:  Print the program version
+
+      """)
   }
 
 }
 
 // MARK: - Hand off to the GeneratorPlugin
 
-// Drop the program name off to get the arguments only.
-let args: [String] = [String](CommandLine.arguments.dropFirst(1))
 let plugin = GeneratorPlugin()
-let result = plugin.run(args: args)
-exit(result)
+plugin.main(nil)
