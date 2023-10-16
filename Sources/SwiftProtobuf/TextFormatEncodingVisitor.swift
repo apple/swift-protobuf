8a0a470d7e127bef4cee27af60f86f05defd8255
Michael Rebello <me@michaelrebello.com>
2023-10-16 05:51:14 -0700

    Update TextFormatEncodingVisitor.swift

--- Sources/SwiftProtobuf/TextFormatEncodingVisitor.swift
+++ Sources/SwiftProtobuf/TextFormatEncodingVisitor.swift
@@ -504,15 +504,15 @@ internal struct TextFormatEncodingVisitor: Visitor {
   // introducing the baggage of a separate option.
 
   private mutating func iterateAndEncode<T>(
-    value: [T], fieldNumber: Int,
+    packedValue: [T], fieldNumber: Int,
     encode: (T, inout TextFormatEncoder) -> ()
   ) throws {
-      assert(!value.isEmpty)
+      assert(!packedValue.isEmpty)
       emitFieldName(lookingUp: fieldNumber)
       encoder.startRegularField()
       var firstItem = true
       encoder.startArray()
-      for v in value {
+      for v in packedValue {
           if !firstItem {
               encoder.arraySeparator()
           }
@@ -524,42 +524,42 @@ internal struct TextFormatEncodingVisitor: Visitor {
   }
 
   mutating func visitPackedFloatField(value: [Float], fieldNumber: Int) throws {
-    try iterateAndEncode(value: value, fieldNumber: fieldNumber) {
+    try iterateAndEncode(packedValue: value, fieldNumber: fieldNumber) {
       (v: Float, encoder: inout TextFormatEncoder) in
       encoder.putFloatValue(value: v)
     }
   }
 
   mutating func visitPackedDoubleField(value: [Double], fieldNumber: Int) throws {
-    try iterateAndEncode(value: value, fieldNumber: fieldNumber) {
+    try iterateAndEncode(packedValue: value, fieldNumber: fieldNumber) {
       (v: Double, encoder: inout TextFormatEncoder) in
       encoder.putDoubleValue(value: v)
     }
   }
 
   mutating func visitPackedInt32Field(value: [Int32], fieldNumber: Int) throws {
-    try iterateAndEncode(value: value, fieldNumber: fieldNumber) {
+    try iterateAndEncode(packedValue: value, fieldNumber: fieldNumber) {
       (v: Int32, encoder: inout TextFormatEncoder) in
       encoder.putInt64(value: Int64(v))
     }
   }
 
   mutating func visitPackedInt64Field(value: [Int64], fieldNumber: Int) throws {
-    try iterateAndEncode(value: value, fieldNumber: fieldNumber) {
+    try iterateAndEncode(packedValue: value, fieldNumber: fieldNumber) {
       (v: Int64, encoder: inout TextFormatEncoder) in
       encoder.putInt64(value: v)
     }
   }
 
   mutating func visitPackedUInt32Field(value: [UInt32], fieldNumber: Int) throws {
-    try iterateAndEncode(value: value, fieldNumber: fieldNumber) {
+    try iterateAndEncode(packedValue: value, fieldNumber: fieldNumber) {
       (v: UInt32, encoder: inout TextFormatEncoder) in
       encoder.putUInt64(value: UInt64(v))
     }
   }
 
   mutating func visitPackedUInt64Field(value: [UInt64], fieldNumber: Int) throws {
-    try iterateAndEncode(value: value, fieldNumber: fieldNumber) {
+    try iterateAndEncode(packedValue: value, fieldNumber: fieldNumber) {
       (v: UInt64, encoder: inout TextFormatEncoder) in
       encoder.putUInt64(value: v)
     }
@@ -590,14 +590,14 @@ internal struct TextFormatEncodingVisitor: Visitor {
   }
 
   mutating func visitPackedBoolField(value: [Bool], fieldNumber: Int) throws {
-    try iterateAndEncode(value: value, fieldNumber: fieldNumber) {
+    try iterateAndEncode(packedValue: value, fieldNumber: fieldNumber) {
       (v: Bool, encoder: inout TextFormatEncoder) in
       encoder.putBoolValue(value: v)
     }
   }
 
   mutating func visitPackedEnumField<E: Enum>(value: [E], fieldNumber: Int) throws {
-    try iterateAndEncode(value: value, fieldNumber: fieldNumber) {
+    try iterateAndEncode(packedValue: value, fieldNumber: fieldNumber) {
       (v: E, encoder: inout TextFormatEncoder) in
       encoder.putEnumValue(value: v)
     }
