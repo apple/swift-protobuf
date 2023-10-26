437f105aac1611dd63f7d45aa756dc4926923bff
Michael Rebello <me@michaelrebello.com>
2023-10-26 11:55:09 -0400

    Add `useDeterministicOrdering` to `JSONEncodingOptions` (#1478)
    * Add `useDeterministicOrdering` to `JSONEncodingOptions`
    
    Adds an option to ensure that JSON serialization is deterministic when serializing Protobuf `map` fields. This should be the only type that needs to be sorted, since individual fields are serialized in order by the generated code that invokes `try visitor.visit(...)` for each field.
    
    Fixes #1477

--- Sources/SwiftProtobuf/JSONEncodingOptions.swift
+++ Sources/SwiftProtobuf/JSONEncodingOptions.swift
@@ -22,5 +22,19 @@ public struct JSONEncodingOptions {
   /// By default they are converted to JSON(lowerCamelCase) names.
   public var preserveProtoFieldNames: Bool = false
 
+  /// Whether to use deterministic ordering when serializing.
+  ///
+  /// Note that the deterministic serialization is NOT canonical across languages. 
+  /// It is NOT guaranteed to remain stable over time. It is unstable across 
+  /// different builds with schema changes due to unknown fields. Users who need
+  /// canonical serialization (e.g., persistent storage in a canonical form,
+  /// fingerprinting, etc.) should define their own canonicalization specification
+  /// and implement their own serializer rather than relying on this API.
+  ///
+  /// If deterministic serialization is requested, map entries will be sorted
+  /// by keys in lexographical order. This is an implementation detail
+  /// and subject to change.
+  public var useDeterministicOrdering: Bool = false
+
   public init() {}
 }
