81b715ce4e7c7dd7cc13b97a9d611f1b59a1a220
Thomas Van Lenten <thomasvl@google.com>
2023-10-27 12:29:08 -0400

    Remove the bridging method without `BinaryEncodingOptions`.
    Since `main` is going to be 2.0, it is accepting breaking changes, so we don't
    need to keep the shim without the argument, we can just have the newer signature
    with the default provided there.

--- Sources/SwiftProtobuf/Message+BinaryAdditions.swift
+++ Sources/SwiftProtobuf/Message+BinaryAdditions.swift
@@ -24,22 +24,6 @@ extension Message {
   ///     `Message.isInitialized` before encoding to verify that all required
   ///     fields are present. If any are missing, this method throws.
   ///     `BinaryEncodingError.missingRequiredFields`.
-  /// - Returns: A `SwiftProtobufContiguousBytes` instance containing the binary serialization
-  /// of the message.
-  ///
-  /// - Throws: `BinaryEncodingError` if encoding fails.
-  public func serializedBytes<Bytes: SwiftProtobufContiguousBytes>(partial: Bool = false) throws -> Bytes {
-    try serializedBytes(partial: partial, options: BinaryEncodingOptions())
-  }
-
-  /// Returns a `SwiftProtobufContiguousBytes` instance containing the Protocol Buffer binary
-  /// format serialization of the message.
-  ///
-  /// - Parameters:
-  ///   - partial: If `false` (the default), this method will check
-  ///     `Message.isInitialized` before encoding to verify that all required
-  ///     fields are present. If any are missing, this method throws.
-  ///     `BinaryEncodingError.missingRequiredFields`.
   ///   - options: The `BinaryEncodingOptions` to use.
   /// - Returns: A `SwiftProtobufContiguousBytes` instance containing the binary serialization
   /// of the message.
@@ -47,7 +31,7 @@ extension Message {
   /// - Throws: `BinaryEncodingError` if encoding fails.
   public func serializedBytes<Bytes: SwiftProtobufContiguousBytes>(
     partial: Bool = false,
-    options: BinaryEncodingOptions
+    options: BinaryEncodingOptions = BinaryEncodingOptions()
   ) throws -> Bytes {
     if !partial && !isInitialized {
       throw BinaryEncodingError.missingRequiredFields
