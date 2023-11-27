eaa06664d65232527178eb56c6959b037b0bfa99
Pouya Yarandi <pouya.yarandi@divar.ir>
2023-11-27 22:54:06 +0330

    Some minor changes in comments

--- Sources/SwiftProtobuf/Message+Applying.swift
+++ Sources/SwiftProtobuf/Message+Applying.swift
@@ -12,29 +12,39 @@
 ///
 // -----------------------------------------------------------------------------
 
-public extension Message {
+/// Applying a value to a field of the message by its field number
+extension Message {
 
-  /// Applies a value for a specific `fieldNumber` of the message
-  /// and returns a copy of it.
+  /// Applies a value for a specific `fieldNumber` of the message and returns a copy
+  /// of the original message.
   ///
   /// - Parameters:
   ///   - value: The value to be applied.
   ///   - fieldNumber: Protobuf index of the field that the value should be applied for.
   /// - Returns: A copy of the message with applied value.
-  func applying(_ value: Any, for fieldNumber: Int) throws -> Self {
+  public func applying(
+    _ value: Any,
+    for fieldNumber: Int
+  ) throws -> Self {
     var copy = self
     try copy.apply(value, for: fieldNumber)
     return copy
   }
 
-  /// Applies a value for a specific `fieldNumber` of the message without
-  /// making a copy. This method mutates the original message.
+  /// Applies a value for a specific `fieldNumber` of the message without making a
+  /// copy. This method mutates the original message.
   ///
   /// - Parameters:
   ///   - value: The value to be applied.
   ///   - fieldNumber: Protobuf index of the field that the value should be applied for.
-  mutating func apply(_ value: Any, for fieldNumber: Int) throws {
-    var decoder = ApplyingDecoder(fieldNumber: fieldNumber, value: value)
+  public mutating func apply(
+    _ value: Any,
+    for fieldNumber: Int
+  ) throws {
+    var decoder = ApplyingDecoder(
+      fieldNumber: fieldNumber,
+      value: value
+    )
     try decodeMessage(decoder: &decoder)
   }
 }
