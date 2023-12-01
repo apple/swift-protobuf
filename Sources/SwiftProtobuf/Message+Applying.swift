9e33a9bc21a9f228927f392db3b43298eaf5bfbd
Pouya Yarandi <pouya.yarandi@divar.ir>
2023-12-01 14:51:10 +0330

    Revert "Some minor changes in comments"
    This reverts commit eaa06664d65232527178eb56c6959b037b0bfa99.

--- Sources/SwiftProtobuf/Message+Applying.swift
+++ Sources/SwiftProtobuf/Message+Applying.swift
@@ -12,39 +12,29 @@
 ///
 // -----------------------------------------------------------------------------
 
-/// Applying a value to a field of the message by its field number
-extension Message {
+public extension Message {
 
-  /// Applies a value for a specific `fieldNumber` of the message and returns a copy
-  /// of the original message.
+  /// Applies a value for a specific `fieldNumber` of the message
+  /// and returns a copy of it.
   ///
   /// - Parameters:
   ///   - value: The value to be applied.
   ///   - fieldNumber: Protobuf index of the field that the value should be applied for.
   /// - Returns: A copy of the message with applied value.
-  public func applying(
-    _ value: Any,
-    for fieldNumber: Int
-  ) throws -> Self {
+  func applying(_ value: Any, for fieldNumber: Int) throws -> Self {
     var copy = self
     try copy.apply(value, for: fieldNumber)
     return copy
   }
 
-  /// Applies a value for a specific `fieldNumber` of the message without making a
-  /// copy. This method mutates the original message.
+  /// Applies a value for a specific `fieldNumber` of the message without
+  /// making a copy. This method mutates the original message.
   ///
   /// - Parameters:
   ///   - value: The value to be applied.
   ///   - fieldNumber: Protobuf index of the field that the value should be applied for.
-  public mutating func apply(
-    _ value: Any,
-    for fieldNumber: Int
-  ) throws {
-    var decoder = ApplyingDecoder(
-      fieldNumber: fieldNumber,
-      value: value
-    )
+  mutating func apply(_ value: Any, for fieldNumber: Int) throws {
+    var decoder = ApplyingDecoder(fieldNumber: fieldNumber, value: value)
     try decodeMessage(decoder: &decoder)
   }
 }
