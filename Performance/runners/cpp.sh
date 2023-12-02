9a75492447120716a7c6386b4fdbc55cc4303ffa
Tim Kientzle <tkientzle@apple.com>
2023-12-01 16:39:29 -0800

    Use CMake to build libprotobuf and the C++ harness
    This reuses some cmake logic from the protobuf dir,
    which isn't ideal, but at least CMake knows how to handle
    abseil.  <whew!>
    
    Note:  This now expects abseil and googletests to be
    already installed on the local system.  A more ambitious
    setup here could potentially avoid that.

--- Performance/runners/cpp.sh
+++ Performance/runners/cpp.sh
@@ -27,24 +27,12 @@ function run_cpp_harness() {
     generate_cpp_harness
 
     echo "Building C++ test harness..."
-    time ( g++ --std=c++11 -O \
-        -o "$harness" \
-        -I "$script_dir" \
-        -I "$GOOGLE_PROTOBUF_CHECKOUT/src" \
-        -L "$GOOGLE_PROTOBUF_CHECKOUT/src/.libs" \
-        -lprotobuf \
-        "$gen_harness_path" \
-        "$script_dir/Harness.cc" \
-        "$script_dir/_generated/message.pb.cc" \
-        "$script_dir/main.cc" \
-    )
-    echo
 
-    # Make sure the dylib is loadable from the harness if the user hasn't
-    # actually installed them.
-    cp "$GOOGLE_PROTOBUF_CHECKOUT"/src/.libs/libprotobuf.*.dylib \
-        "$script_dir/_generated"
-
+    pushd $script_dir/runners
+    cmake -B ../_generated -S .
+    cmake --build ../_generated
+    popd
+    
     run_harness_and_concatenate_results "C++" "$harness" "$partial_results"
   )
 }
