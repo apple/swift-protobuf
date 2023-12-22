5891e5c99c0cfc19bd808b6888bcd0709b21cdc4
Michael Rebello <me@michaelrebello.com>
2023-12-21 17:29:38 -0800

    Make `anyTranscodeFailure` more descriptive
    Right now it's quite difficult to identify _which type_ is missing from the `Google_Protobuf_Any` registry when the `anyTranscodeFailure` is thrown without having Xcode and breakpoints to debug the program. This change makes the error more descriptive and actionable by including the type URL that needs to be registered.

--- Sources/SwiftProtobuf/JSONEncodingError.swift
+++ Sources/SwiftProtobuf/JSONEncodingError.swift
@@ -12,12 +12,12 @@
 ///
 // -----------------------------------------------------------------------------
 
-public enum JSONEncodingError: Error {
+public enum JSONEncodingError: Error, Equatable {
     /// Any fields that were decoded from binary format cannot be
     /// re-encoded into JSON unless the object they hold is a
     /// well-known type or a type registered with via
     /// Google_Protobuf_Any.register()
-    case anyTranscodeFailure
+    case anyTypeURLNotRegistered(typeURL: String)
     /// Timestamp values can only be JSON encoded if they hold a value
     /// between 0001-01-01Z00:00:00 and 9999-12-31Z23:59:59.
     case timestampRange
