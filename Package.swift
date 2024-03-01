36f58f868a23b4f1f94ca41f87b232abdd679a6d
Tim Kientzle <tkientzle@apple.com>
2024-03-01 09:24:54 -0800

    [1_x_release_branch] Include "empty" privacy manifest file
    Include it as a resource in both SwiftProtobuf library
    and SwiftProtobufPluginLibrary when built via podspec or swiftpm
    
    Cherry-pick from #1565

--- Package.swift
+++ Package.swift
@@ -19,8 +19,10 @@ let package = Package(
     .library(name: "SwiftProtobufPluginLibrary", targets: ["SwiftProtobufPluginLibrary"]),
   ],
   targets: [
-    .target(name: "SwiftProtobuf"),
+    .target(name: "SwiftProtobuf",
+            resources: [.copy("PrivacyInfo.xcprivacy")]),
     .target(name: "SwiftProtobufPluginLibrary",
+            resources: [.copy("PrivacyInfo.xcprivacy")],
             dependencies: ["SwiftProtobuf"]),
     .target(name: "protoc-gen-swift",
             dependencies: ["SwiftProtobufPluginLibrary", "SwiftProtobuf"]),
