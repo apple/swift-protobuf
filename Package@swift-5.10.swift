bfcd341710afaa97244d3cf2dfbec17dea88b687
Thomas Van Lenten <thomasvl@google.com>
2024-03-14 10:16:31 -0400

    Add swift 5.10 to CI to the 1.x branch.
    Add the 5.9 and 5.10 Package.swift files also (copies of the 5.8).

---
+++ Package@swift-5.10.swift
@@ -0,0 +1,48 @@
+// swift-tools-version:5.6
+
+// Package.swift
+//
+// Copyright (c) 2014 - 2018 Apple Inc. and the project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See LICENSE.txt for license information:
+// https://github.com/apple/swift-protobuf/blob/main/LICENSE.txt
+//
+
+import PackageDescription
+
+let package = Package(
+  name: "SwiftProtobuf",
+  products: [
+    .executable(name: "protoc-gen-swift", targets: ["protoc-gen-swift"]),
+    .library(name: "SwiftProtobuf", targets: ["SwiftProtobuf"]),
+    .library(name: "SwiftProtobufPluginLibrary", targets: ["SwiftProtobufPluginLibrary"]),
+    .plugin(
+        name: "SwiftProtobufPlugin",
+        targets: ["SwiftProtobufPlugin"]
+    ),
+  ],
+  targets: [
+    .target(name: "SwiftProtobuf",
+            resources: [.copy("PrivacyInfo.xcprivacy")]),
+    .target(name: "SwiftProtobufPluginLibrary",
+            dependencies: ["SwiftProtobuf"],
+            resources: [.copy("PrivacyInfo.xcprivacy")]),
+    .executableTarget(name: "protoc-gen-swift",
+            dependencies: ["SwiftProtobufPluginLibrary", "SwiftProtobuf"]),
+    .executableTarget(name: "Conformance",
+            dependencies: ["SwiftProtobuf"]),
+    .plugin(
+        name: "SwiftProtobufPlugin",
+        capability: .buildTool(),
+        dependencies: [
+            "protoc-gen-swift"
+        ]
+    ),
+    .testTarget(name: "SwiftProtobufTests",
+                dependencies: ["SwiftProtobuf"]),
+    .testTarget(name: "SwiftProtobufPluginLibraryTests",
+                dependencies: ["SwiftProtobufPluginLibrary"]),
+  ],
+  swiftLanguageVersions: [.v4, .v4_2, .version("5")]
+)
