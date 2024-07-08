d57a5aecf24a25b32ec4a74be2f5d0a995a47c4b
Thomas Van Lenten <thomasvl@google.com>
2024-07-08 10:11:00 -0400

    Move `main` to 1.27.0
    The next release will be 1.27.0 and will come off `main`.

--- Sources/SwiftProtobuf/Version.swift
+++ Sources/SwiftProtobuf/Version.swift
@@ -17,9 +17,9 @@ import Foundation
 // Expose version information about the library.
 public struct Version {
   /// Major version.
-  public static let major = 2
+  public static let major = 1
   /// Minor version.
-  public static let minor = 0
+  public static let minor = 27
   /// Revision number.
   public static let revision = 0
 
