8d3356266bbf57b791eb19997d10253106182568
Thomas Van Lenten <thomasvl@google.com>
2025-01-07 09:18:32 -0500

    Use Swift 6 mode on the plugin examples to ensure generated code compiles in this mode (#1740)

--- PluginExamples/Package@swift-5.9.swift
+++ PluginExamples/Package@swift-5.9.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.9
 
 import PackageDescription
 
@@ -56,6 +56,5 @@ let package = Package(
                 .target(name: "AccessLevelOnImport"),
             ]
         ),
-    ],
-    swiftLanguageModes: [.v6]
+    ]
 )
