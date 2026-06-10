68914ad6ce44a9a4264ca3ce1ed026ba920c7f63
Tony Allevato <allevato@google.com>
2026-06-10 11:31:08 -0400

    Address review feedback.

--- Tests/SwiftProtobufTests/Test_Leaks.swift
+++ Tests/SwiftProtobufTests/Test_Leaks.swift
@@ -1,4 +1,4 @@
-// Tests/SwiftProtobufTests/Test_Leaks.swift - Proto3 coding/decoding
+// Tests/SwiftProtobufTests/Test_Leaks.swift - Memory leak detection
 //
 // Copyright (c) 2014 - 2026 Apple Inc. and the project authors
 // Licensed under Apache License v2.0 with Runtime Library Exception
