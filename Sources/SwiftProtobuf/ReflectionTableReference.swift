40887db111b1f2064a8856036b361353f65d3b44
Tony Allevato <allevato@google.com>
2026-08-24 06:45:12 -0700

    [table-driven-branch] Add our own `Lock` that doesn't require Foundation. (#2153)
    This is adapted from `NIOLock` in swift-nio, but using `os_unfair_lock`
    on Apple platforms instead of `pthread_mutex`.
    
    Also re-enable the no-Foundation linkage test.

--- Sources/SwiftProtobuf/ReflectionTableReference.swift
+++ Sources/SwiftProtobuf/ReflectionTableReference.swift
@@ -13,60 +13,60 @@
 ///
 // -----------------------------------------------------------------------------
 
-// TODO: Use this when not always using NSLock
-//#if canImport(FoundationEssentials)
-//import FoundationEssentials
-//#else
-//import Foundation
-//#endif
-import Foundation
-
 /// A reference to a reflection table, which may be inlined (embedded in the generated code) or
 /// stored as a compressed buffer.
 final class ReflectionTableReference: @unchecked Sendable {
-    private enum State {
-        case compressed(UnsafeRawBufferPointer, fieldCount: Int)
-        case direct(ReflectionTable)
-    }
+    /// The compressed reflection table data, if any.
+    private let compressed: UnsafeRawBufferPointer?
+
+    /// The number of fields in the message whose reflection table is being referenced.
+    private let fieldCount: Int
 
     /// A singleton instance that references the fixed reflection table for map entries.
     static let mapEntry: ReflectionTableReference = .init(direct: .mapEntry)
 
-    /// Guards `state`.
-    private let lock = NSLock()
+    /// Guards `cachedTable` during decompression.
+    private let lock = Lock()
 
-    /// The state of the table, either compressed or decompressed.
-    private var state: State
+    /// The cached decompressed reflection table.
+    private var cachedTable: ReflectionTable?
 
     /// Creates a new reflection table reference from a compressed buffer.
     ///
     /// The pointer to the compressed data is assumed to be immortal (either static data in
     /// the binary or allocated in a pool that lives for the lifetime of the reference).
     init(compressed: UnsafeRawBufferPointer, fieldCount: Int) {
-        self.state = .compressed(compressed, fieldCount: fieldCount)
+        self.compressed = compressed
+        self.fieldCount = fieldCount
+        self.cachedTable = nil
     }
 
     /// Creates a new reference to an existing reflection table.
     private init(direct table: ReflectionTable) {
-        self.state = .direct(table)
+        self.compressed = nil
+        self.fieldCount = 0
+        self.cachedTable = table
     }
 
     /// Calls the given body with the reflection table, decompressing it on the
     /// first call if needed.
     func withTable<R>(_ body: (borrowing ReflectionTable) throws -> R) rethrows -> R {
-        try lock.withLock {
-            switch state {
-            case .direct(let table):
-                return try body(table)
-
-            case .compressed(let buffer, let fieldCount):
-                let table = ReflectionTable(
-                    fieldCount: fieldCount,
-                    data: Compression.decompress(buffer)
-                )
-                state = .direct(table)
-                return try body(table)
+        let table: ReflectionTable = lock.withLock {
+            if let table = cachedTable {
+                return table
             }
+
+            guard let compressed else {
+                fatalError("Corrupted state: compressed buffer missing")
+            }
+
+            let table = ReflectionTable(
+                fieldCount: fieldCount,
+                data: Compression.decompress(compressed)
+            )
+            cachedTable = table
+            return table
         }
+        return try body(table)
     }
 }
