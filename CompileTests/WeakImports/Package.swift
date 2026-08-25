f8b2fb2b77cedf9058a3a0ddc804b88fa145dd68
Tony Allevato <allevato@google.com>
2026-08-25 12:40:29 -0400

    [table-driven-branch] Add `-internalize-at-link` to the WeakImports test and splat out some symbol presence checks that we'll burn down as the implementation progresses.

--- CompileTests/WeakImports/Package.swift
+++ CompileTests/WeakImports/Package.swift
@@ -2,6 +2,16 @@
 
 import PackageDescription
 
+let swiftSettings: [SwiftSetting] = [
+    // Ensure that public symbols are not unconditionally `llvm.used` so that
+    // the linker can strip them from the final binary.
+    //
+    // This isn't a standard build configuration, but clients who care enough
+    // about minimizing binary size to use weak imports would likely also be
+    // setting this.
+    .unsafeFlags(["-Xfrontend", "-internalize-at-link"]),
+]
+
 let package = Package(
     name: "WeakImports",
     dependencies: [
@@ -14,25 +24,29 @@ let package = Package(
                 .target(name: "ModuleB"),
                 .target(name: "ModuleC"),
                 .product(name: "SwiftProtobuf", package: "swift-protobuf"),
-            ]
+            ],
+            swiftSettings: swiftSettings
         ),
         .target(
             name: "ModuleB",
             dependencies: [
                 .product(name: "SwiftProtobuf", package: "swift-protobuf")
-            ]
+            ],
+            swiftSettings: swiftSettings
         ),
         .target(
             name: "ModuleC",
             dependencies: [
                 .product(name: "SwiftProtobuf", package: "swift-protobuf")
-            ]
+            ],
+            swiftSettings: swiftSettings
         ),
         .executableTarget(
             name: "Client",
             dependencies: [
                 .target(name: "ModuleA")
-            ]
+            ],
+            swiftSettings: swiftSettings
         ),
     ]
 )
